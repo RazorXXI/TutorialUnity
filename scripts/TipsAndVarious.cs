cdd60e9eb13de313e51ac34ce346a3dd2f8f1e17
Mr.Hidden <juanl.romeroduenas@gmail.com>
2022-02-12 15:05:50 +0100

    Update TipsAndVarious.cs

--- scripts/TipsAndVarious.cs
+++ scripts/TipsAndVarious.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class TipsAndVarious : MonoBehaviour
 {
-    // Start is called before the first frame update
+   // Start is called before the first frame update
     void Start()
     {
+        //Llamando con Invoke
+        //Invoke(nameof(SayHelloWithInvoke), 10f);
+        //Invoke("SayHelloWithInvoke", 5f);
+
+        InvokeRepeating(nameof(SayHelloPepe), 3f, 5f);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Invoke Detenidos...");
+            CancelInvoke();
+        }
+    }
+    //Vamos a usar Invoke
+    void SayHelloWithInvoke()
+    {
+        Debug.Log("Te estoy diciendo Hola con Invoke");
+    }
+
+    IEnumerator SayHelloWithParameter(string nameToSayHello)
+    {
+        yield return new WaitForSeconds(3f);
+        Debug.Log("Hello " + nameToSayHello);
+    }
+
+    void SayHelloPepe()
+    {
+        Debug.Log("Hello Pepe Again...");
     }
 }
