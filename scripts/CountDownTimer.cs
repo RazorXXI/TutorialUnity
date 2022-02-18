1d1d40436a8f537e5a637c8be79f1f9944b77814
Mr.Hidden <juanl.romeroduenas@gmail.com>
2022-02-18 03:11:07 +0100

    Update CountDownTimer.cs

--- scripts/CountDownTimer.cs
+++ scripts/CountDownTimer.cs
@@ -5,34 +5,34 @@ using UnityEngine.UI;
 
 public class CountDownTimer : MonoBehaviour
 {
-    [SerializeField] int minutes;
-    [SerializeField] float seconds;
-    [SerializeField] bool countDownFinish;
-    [SerializeField] Text counterText;
+    [SerializeField] int minutes;           //Para indicar los minutos de la cuenta atrás
+    [SerializeField] float seconds;         //Para indicar los segundos de la cuenta atrás
+    [SerializeField] bool countDownFinish;  //Para la condición de finalización del contador
+    [SerializeField] Text counterText;      //Para la referencia del canvas
 
     // Update is called once per frame
     void Update()
     {
-        CountDownExecuter();
-        CountDownUpdateText();
+        CountDownExecuter();    //Ejecuta la cuenta atrás
+        CountDownUpdateText();  //Muestra la cuenta atrás en el canvas
     }
 
     void CountDownExecuter()
     {
-        int sec;
+        int sec;    //Variable para poder controlar el conteo de segundos
 
-        if (!countDownFinish)
+        if (!countDownFinish)   //Comprobamos que no haya finalizado el contador
         {
-            seconds -= Time.deltaTime;
-            sec = (int)seconds;
+            seconds -= Time.deltaTime; //Vamos descontando segundos
+            sec = (int)seconds; //Convertimos el valor devuelto en segundos a tipo entero para asignarlo a la variable sec
 
             if (sec == 0) //Hemos llegado a 0 segundos 
             {
-                if (minutes == 0) countDownFinish = true;
+                if (minutes == 0) countDownFinish = true;  //Si hemos llegado a 0 minutos hemos terminado
                 else
                 {
-                    minutes--;
-                    seconds = 60;
+                    minutes--;      //Descontamos una unidad a los minutos
+                    seconds = 60;   //Reiniciamos los segundos
                 }
             }
 
@@ -43,7 +43,7 @@ public class CountDownTimer : MonoBehaviour
     {
         int sec;
         sec = (int)seconds;
-        if (sec<10) counterText.text = minutes.ToString() + ":0" + sec.ToString();
+        if (sec<10) counterText.text = minutes.ToString() + ":0" + sec.ToString(); //Esta linea es para que no se vea feo al bajar de 0
         else counterText.text = minutes.ToString() + ":" + sec.ToString();
     }
 }
