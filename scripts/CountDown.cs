98e200515f07d555452f31eb352e6b49aad98b45
Mr.Hidden <juanl.romeroduenas@gmail.com>
2022-02-18 02:26:22 +0100

    Create CountDown.cs

---
+++ scripts/CountDown.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    [SerializeField] float countDownTime; //Para el tiempo que va a realizar la cuenta atrás
+    [SerializeField] bool countDownFinished; //Para indicar que a terminado la cuenta atrás
+    [SerializeField] Text countDownText; //Para referenciar el texto del canvas
+
+    void Update()
+    {
+        CountDownExecuter(); //Función que va a realizar la cuenta atrás
+    }
+
+    void CountDownExecuter() 
+    {
+        int valueTime;
+        if (!countDownFinished) //Condición para controlar que aun no a terminado la cuenta atrás
+        {
+            countDownTime -= Time.deltaTime;
+            valueTime = (int)countDownTime;
+            countDownText.text = valueTime.ToString();//Actualizamos el valor text del componente CountDownTex con valueTime
+            //valueTime llama a su método ToString, para convertirlo, dado que valueTime es de tipo int y countDownText de tipo string
+
+            if (valueTime == 0) countDownFinished = true;
+        }
+    }
+}
