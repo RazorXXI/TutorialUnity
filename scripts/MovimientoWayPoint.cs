8f15ae6bcda807b2b3a4c8cb3100107cc97f1df5
Mr.Hidden <juanl.romeroduenas@gmail.com>
2022-07-10 19:03:01 +0200

    Create MovimientoWayPoint.cs

---
+++ scripts/MovimientoWayPoint.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+//Este script, lo podemos aplicar al comportamiento de un enemigo en nuestro juego, para que se mueva a lo largo de una trayectora predefinida
+public class MovimientoWayPoint : MonoBehaviour
+{
+    [SerializeField] Transform[] wayPoints;             //Array que almacenara los WayPoints que hayamos dispuesto en el escenario para el enemigo
+    [SerializeField] float movementSpeed;               //La velocidad a la que se desplazara de un waypoint a otro
+    [SerializeField] float distanciaCambioPunto;        //Distancia a la que cambiara a otro punto de la ruta
+
+
+    //Funcion que mueve un objeto, en una ruta predefinida por puntos. Esto seran empty objects ubicados en el escenario
+    void MoverPorWayPoints()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, wayPoints[siguientePunto].transform.position, movementSpeed * Time.deltaTime);
+        if(Vector3.Distance(transform.position, wayPoints[siguientePunto].transform.position) < distanciaCambioPunto)
+        {
+            siguientePunto++;
+            if(siguientePunto >= wayPoints.Length) siguientePunto = 1;
+        }
+    }
+}
